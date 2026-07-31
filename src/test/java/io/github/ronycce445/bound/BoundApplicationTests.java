0bce6f3c466096fd29e4f599f35c3d3a46b4b756
Koustav Chakraborty <ckoustav7@gmail.com>
2026-07-31 20:41:50 +0530

    chore: initialize Bound Spring Boot project

---
+++ src/test/java/io/github/ronycce445/bound/BoundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ronycce445.bound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
